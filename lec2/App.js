ec9bb7d9f320afbecc50e70e88640e708fbafcf6
amit05sept <gladiatorpandey@gmail.com>
2023-10-25 17:17:24 +0530

    Lecture-2 complet

---
+++ lec2/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const heading1 = React.createElement("h1",{},"THIS IS BASIC REACT APP");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// if we want to make something like this 
+// div>heading1 + heading2
+const heading2 = React.createElement("h2",{},"THIS is heading2");
+
+const divContainer = React.createElement("div",{id:"container",style:{border:"4px solid black"}},[heading1,heading2]);
+root.render(divContainer);
+
